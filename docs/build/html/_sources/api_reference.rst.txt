736920f940abb0ee8e505baf0ac926249dcb20eb
nellipestych <NelliPestych@bigmir.net>
2025-06-17 16:18:27 +0300

    homework 12 code refactoring

--- docs/build/html/_sources/api_reference.rst.txt
+++ docs/build/html/_sources/api_reference.rst.txt
@@ -1,29 +1,97 @@
 API Reference
 =============
 
+Main Application Endpoints
+--------------------------
+
 .. automodule:: src.main
+   :exclude-members: read_root, startup
    :members:
+
+Initial Application Setup
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+.. autofunction:: src.main.startup
+
+Root Endpoint for API Status
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+.. autofunction:: src.main.read_root
+
+Authentication Core Functions
+-----------------------------
+
+.. automodule:: src.auth
+   :members:
+   :undoc-members:
+   :show-inheritance:
 
 Authentication Endpoints
 ------------------------
 
-.. automodule:: src.routers.auth
+.. automodule:: src.routers.auth_routes
    :members:
+   :undoc-members:
+   :show-inheritance:
 
 Contacts Endpoints
 ------------------
 
 .. automodule:: src.routers.contacts
    :members:
+   :undoc-members:
+   :show-inheritance:
 
 Users Endpoints
 ---------------
 
 .. automodule:: src.routers.users
    :members:
+   :undoc-members:
+   :show-inheritance:
 
 CRUD Operations
 ---------------
 
 .. automodule:: src.crud
    :members:
+   :undoc-members:
+   :show-inheritance:
+
+Database Utilities
+------------------
+
+.. automodule:: src.database
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+Dependencies
+------------
+
+.. automodule:: src.deps
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+Email Utilities
+---------------
+
+.. automodule:: src.email_utils
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+Models
+------
+
+.. automodule:: src.models
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+Schemas
+-------
+
+.. automodule:: src.schemas
+   :members:
+   :undoc-members:
+   :show-inheritance:
--- docs/source/conf.py
+++ docs/source/conf.py
@@ -10,13 +10,12 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../../src')) # Це важливо! Переконайтеся, що шлях правильний
-                                           # для переходу до кореневої папки вашого проекту.
-                                           # Якщо 'docs/source' знаходиться в корені, то '../..'
-                                           # підніметься на два рівні вгору до кореня проекту.
+# ВИПРАВЛЕНО: Додаємо корінь проекту до Python Path
+# Якщо conf.py знаходиться в docs/source/, то нам потрібно піднятися на два рівні вгору,
+# щоб потрапити до кореня проекту, де знаходиться папка src/.
+sys.path.insert(0, os.path.abspath('../..')) # Правильний шлях для docs/source/conf.py
 
-
-# -- Project information -----------------------------------------------------
+# -- Project information -----------------------------------------------------\
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = 'Contacts App API'
@@ -24,22 +23,44 @@ copyright = '2025, NelliPestych'
 author = 'NelliPestych'
 release = '0.1.0'
 
-# -- General configuration ---------------------------------------------------
+# -- General configuration ---------------------------------------------------\
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
     'sphinx.ext.autodoc',             # Для автоматичної генерації документації з docstrings
     'sphinx.ext.napoleon',            # Для підтримки Google та NumPy стилів docstrings
     'sphinx_rtd_theme',               # Тема "Read the Docs" для документації
-    'sphinx_autodoc_typehints'        # Для правильного відображення типів з анотацій Python
+    'sphinx_autodoc_typehints',       # Для правильного відображення типів з анотацій Python
+    'sphinx.ext.todo',                # Можна додати для підтримки TODO-нотаток в документації
+    'sphinx.ext.viewcode',            # Додає посилання на вихідний код у документації
+]
+
+# ОБОВ'ЯЗКОВО: Додано autodoc_mock_imports для залежностей
+autodoc_mock_imports = [
+    "fastapi",
+    "sqlalchemy",
+    "pydantic",
+    "jose",
+    "passlib",
+    "fastapi_mail",
+    "redis",
+    "cloudinary",
+    "dotenv",
+    "alembic"
 ]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store'] # Ігнорувати ці файли та папки при збірці
 
 
-# -- Options for HTML output -------------------------------------------------
+# -- Options for HTML output -------------------------------------------------\
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'sphinx_rtd_theme' # Встановлюємо тему "Read the Docs"
 html_static_path = ['_static']
+
+# Якщо ви використовуєте Markdown для деяких файлів, розкоментуйте
+# source_suffix = {
+#     '.rst': 'restructuredtext',
+#     '.md': 'markdown',
+# }
